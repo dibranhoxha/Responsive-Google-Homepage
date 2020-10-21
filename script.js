060b8241c028995aeeefd2cb52c3cee248c702b4
dibran_ho <dibran_ho@outlook.com>
2020-10-21 15:01:37 +0200

    added footer section in tablet, modified javascript code to animate smothly buttone bit and created auto-complete-section

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
 let autoCompleteElement = document.getElementById("auto-complete-container");
 let searchBar = document.getElementById("search-bar");
-
+let searchBarContainer = document.getElementById("search-bar-container");
+let animatedButton = document.getElementById('animated-button');
+let animatedList = document.querySelector("#animated-button ul");
+let animatedListItems = document.querySelectorAll("#animated-button ul li");
 
 searchBar.addEventListener("click", () => {
-    autoCompleteElement.id = "auto-complete";
+    autoCompleteElement.id = "iphone-auto-complete";
 })
 
 document.querySelector("#submit-buttons").addEventListener("click", () => {
     autoCompleteElement.id = "none";
 })
 
-document.getElementById('animated-button').addEventListener('mouseenter', () => {
-    let pxArr = [23, 49, 74, 101, 127, 154, 180, 207, 233, 255]
-    let randomNr = Math.floor(Math.random() * (pxArr.length-1));
-    let px = -pxArr[randomNr];
-
+let pxArr = [49, 74, 101, 127, 154, 180, 207, 233, 255]
+let randomNr = Math.floor(Math.random() * (pxArr.length - 1));
+let px = -pxArr[randomNr];
+animatedButton.addEventListener('mouseenter', () => {
     setTimeout(() => {
-        document.querySelector("#animated-button ul").style.marginTop = px + "px";
-    }, 100);
+        animatedList.style.transition = "margin-top 0.9s ease";
+        animatedList.style.marginTop = px + "px";
+    }, 300);
 });
 
-document.getElementById('animated-button').addEventListener('mouseleave', () => {
-    document.querySelector("#animated-button ul").style.marginTop = "4px";
+animatedButton.addEventListener('mouseleave', () => {
+
+    setTimeout(() => {
+        animatedList.style.transition = "margin-top 0s ease";
+        animatedList.style.marginTop = "4px";
+    }, 300);
 });
 
 
