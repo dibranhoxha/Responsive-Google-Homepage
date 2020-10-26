55647e54454ce059f41250ebd9e535b6d57bf659
Besnik Ruhani <besnik@ruhani.de>
2020-10-26 11:19:15 +0100

    Small Fixes

--- script.js
+++ script.js
@@ -4,13 +4,10 @@ let searchBarContainer = document.getElementById("search-bar-container");
 let actionContainer = document.getElementById("action-container")
 let animatedButton = document.getElementById('animated-button');
 let animatedList = document.querySelector("#animated-button ul");
-let animatedListItems = document.querySelectorAll("#animated-button ul li");
+let animatedListItems = document.querySelectorAll("#animated-button > ul > li");
 let iphoneInput = document.querySelector('.input-container .iphone-search-input');
 let iphonebackward = document.getElementById("iphone-backward");
 let desktopBackward = document.getElementById("desktop-backward");
-let pxArr = [49, 74, 101, 127, 154, 180, 207, 233, 255]
-let randomNr = Math.floor(Math.random() * (pxArr.length - 1));
-let px = -pxArr[randomNr];
 
 animatedButton.addEventListener('mouseenter', () => {
     randomSelection();
@@ -33,7 +30,6 @@ desktopBackward.addEventListener("click", () => {
 });
 
 
-
 function appearAutoComplete() {
     autoCompleteElement.id = "device-auto-complete";
     searchBarContainer.id = "auto-complete-search-bar";
@@ -49,10 +45,17 @@ function dissapearAutoComplete() {
     desktopBackward.style.display = "none";
 }
 
+
 function randomSelection() {
+    let elIndex = getRandomInt(1, (animatedListItems.length - 1));
+
+    // let elementPixels = [49, 74, 101, 127, 154, 180, 207, 233, 255]
+    // let randomIndex = Math.floor(Math.random() * (elementPixels.length - 1));
+    // let randomPixel = -elementPixels[randomIndex];
+
     setTimeout(() => {
         animatedList.style.transition = "margin-top 0.9s ease";
-        animatedList.style.marginTop = px + "px";
+        animatedList.style.marginTop = (animatedListItems.offsetTop - animatedListItems[elIndex].offsetTop) + "px";
     }, 300);
 }
 
@@ -66,6 +69,26 @@ function fixedSelection() {
 
 
 
+
+/**
+ * Returns a random number between min (inclusive) and max (exclusive)
+ */
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive).
+ * The value is no lower than min (or the next integer greater than min
+ * if min isn't an integer) and no greater than max (or the next integer
+ * lower than max if max isn't an integer).
+ * Using Math.round() will give you a non-uniform distribution!
+ */
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 
 
