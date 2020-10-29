12d06d38339b9605aca630a0a7f033d536791d30
dibran_ho <dibran_ho@outlook.com>
2020-10-29 16:25:37 +0100

    Changed randomSelection function's logic

--- script.js
+++ script.js
@@ -4,13 +4,11 @@ let searchBarContainer = document.getElementById("search-bar-container");
 let actionContainer = document.getElementById("action-container")
 let animatedButton = document.getElementById('animated-button');
 let animatedList = document.querySelector("#animated-button ul");
-let animatedListItems = document.querySelectorAll("#animated-button ul li");
+let animatedListItems = document.querySelectorAll("#animated-button > ul > li");
 let iphoneInput = document.querySelector('.input-container .iphone-search-input');
 let iphonebackward = document.getElementById("iphone-backward");
 let desktopBackward = document.getElementById("desktop-backward");
-let pxArr = [49, 74, 101, 127, 154, 180, 207, 233, 255]
-let randomNr = Math.floor(Math.random() * (pxArr.length - 1));
-let px = -pxArr[randomNr];
+
 
 animatedButton.addEventListener('mouseenter', () => {
     randomSelection();
@@ -33,7 +31,6 @@ desktopBackward.addEventListener("click", () => {
 });
 
 
-
 function appearAutoComplete() {
     autoCompleteElement.id = "device-auto-complete";
     searchBarContainer.id = "auto-complete-search-bar";
@@ -48,18 +45,56 @@ function dissapearAutoComplete() {
     actionContainer.style.display = "block";
     desktopBackward.style.display = "none";
 }
-
 function randomSelection() {
+    let randomNumber = Math.floor((Math.random() * (animatedListItems.length - 1) + 1) / 2);
+    let randomCase = Math.random() >= 0.5;
+    let ul = window.getComputedStyle(animatedList)
+    let definedMargin = ul.getPropertyValue('margin-top');
+    let number = parseInt(definedMargin.replace(/\D+$/g, ""));
+    let Pixels;
+    if (randomCase) {
+        Pixels = number + (randomNumber * 26);
+    } else {
+        Pixels = number - (randomNumber * 26);
+    }
     setTimeout(() => {
-        animatedList.style.transition = "margin-top 0.9s ease";
-        animatedList.style.marginTop = px + "px";
-    }, 300);
+        if (Pixels !== number) {
+            animatedList.style.transition = "margin-top 0.5s ease";
+            animatedList.style.marginTop = Pixels + "px";
+        } else {
+            randomSelection();
+        }
+    }, 300)
 }
+
+// function randomSelection() {
+//     let randomIndex = Math.floor(Math.random() * (animatedListItems.length - 1));
+//     console.log(animatedListItems[3].offsetTop)
+//     // let randomPixels = -(animatedListItems[randomIndex].offsetTop);
+//     setTimeout(() => {
+//         animatedList.style.transition = "margin-top 0.9s ease";
+//         // animatedList.style.marginTop = randomPixels + "px";
+//     }, 300);
+// }
+
+// function randomSelection() {
+//     // let elIndex = getRandomInt(1, (animatedListItems.length));
+
+//     let elementPixels = [-4, 49, 74, 101, 154, 180, 207, 233, 255]
+//     let randomIndex = Math.floor(Math.random() * (elementPixels.length - 1));
+//     let randomPixels = -elementPixels[randomIndex];
+
+//     setTimeout(() => {
+//         animatedList.style.transition = "margin-top 0.9s ease";
+//         animatedList.style.marginTop = randomPixels + "px";
+//     }, 300);
+// }
+
 
 function fixedSelection() {
     setTimeout(() => {
         animatedList.style.transition = "margin-top 0s ease";
-        animatedList.style.marginTop = "4px";
+        animatedList.style.marginTop = "-130px";
     }, 300);
 }
 
